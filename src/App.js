a27360de58ee9f96112f8927e05313b79f59be17
Lisette Venema <lisettevenema@hotmail.com>
2020-10-24 12:33:21 +0200

    coded along with explanation of Guido, so far so good

--- src/App.js
+++ src/App.js
@@ -1,37 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import contacts from './contacts.json'
+import contactsFromJson from './contacts.json';
 
 // function App() {
 //   console.log(contacts)
 //   return (
 //     <div className="App">
-      
+
 //     </div>
 //   );
 // }
 
-class App extends React.Component{
-  state = {contacts: contacts.slice(0,5)}
+class App extends React.Component {
+  state = { contacts: contactsFromJson.slice(0, 5) };
+
+  addRandomContact = () => {
+    const randomNumber = Math.floor(
+      Math.random() * contactsFromJson.length - 1 + 1
+    );
+    const randomContact = contactsFromJson[randomNumber];
+
+    this.state.contacts.forEach((contact) => {
+      if (!this.state.contacts.includes(randomContact)) {
+        this.setState({
+          contacts: [randomContact, ...this.state.contacts],
+        });
+      } else {
+        console.log('contact is already present');
+      }
+    });
+  };
+
   render() {
-    console.log(this.state.contacts)
+    console.log(this.state.contacts);
     return (
-      <div>
+      <div className="contacts-container">
+        <h1>Iron contacts</h1>
+        <button onClick={this.addRandomContact}>Add random contact</button>
         {this.state.contacts.map((contact) => {
-          return <table>
-                  <img id="photo" src={contact.pictureUrl}/>
-          <li>{contact.name}</li>
-          <li>{contact.popularity}</li>
-          </table>
+          return (
+            <table>
+              <thead>
+                <tr>
+                  <th>Picture</th>
+                  <th>Name</th>
+                  <th>Popularity</th>
+                </tr>
+              </thead>
+              <tr key={contact.id}></tr>
+              <td>
+                <img id="photo" src={contact.pictureUrl} />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+            </table>
+          );
         })}
-
-
-        <button>
-          add random
-        </button>
       </div>
-    )
+    );
   }
 }
 
